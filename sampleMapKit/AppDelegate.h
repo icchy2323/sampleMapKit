5e29244d6aef0044e3bab65b3bdff25bd4538783
市川 貴章 <icchy.2323@gmail.com>
2015-02-06 13:27:21 +0900

    追加

---
+++ sampleMapKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleMapKit
+//
+//  Created by 市川 貴章 on 2015/01/19.
+//  Copyright (c) 2015年 市川 貴章. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleMapKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sampleMapKit
+//
+//  Created by 市川 貴章 on 2015/01/19.
+//  Copyright (c) 2015年 市川 貴章. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
